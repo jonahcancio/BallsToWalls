1e1ae57337f0fcf9038314704160486afdb87266
jonahcancio <jonahcancio@gmail.com>
2020-11-16 22:00:56 +0800

    Updated Ball visuals

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    Dictionary<string, Color> stateColors;
-    public Color lifeColor;
-    public Color deathColor;
+    public List<BallState> stateList;
+
+
+    [System.Serializable]
+    public class BallState
+    {
+        public Color color;
+        public Sprite sprite;
+    }
+
     public float baseDamage = 10f;
 
     [SerializeField]
@@ -14,18 +21,17 @@ public class Ball : MonoBehaviour
 
     public void OnSpawn()
     {
-        stateColors = new Dictionary<string, Color>();
-        stateColors["life"] = lifeColor;
-        stateColors["death"] = deathColor;
-
         ChangeState("death");
     }
 
     public void ChangeState(string state)
     {
         currentState = state;
-        GetComponent<SpriteRenderer>().color = stateColors[state];
         baseDamage = state == "death" ? 15 : -5;
+        int stateIndex = state == "death" ? 1 : 0;
+        GetComponent<SpriteRenderer>().color = stateList[stateIndex].color;
+        GetComponent<SpriteRenderer>().sprite = stateList[stateIndex].sprite;
+
     }
 
     public void FlipState()
